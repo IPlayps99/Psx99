f2c0a59c4b46536d844ba7bf2b4600dc30c9b268
IPlayps99 <155312670+IPlayps99@users.noreply.github.com>
2024-01-04 01:01:30 +0000

    Update SnipeNoCap.lua

--- SnipeNoCap.lua
+++ SnipeNoCap.lua
@@ -144,7 +144,7 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
     end
 
     local price = gems / amount
-    task.wait(3.05)
+    task.wait(3.02)
     if type.huge and price <= 5000000 then	
         local boughtPet, boughtMessage = purchase:InvokeServer(playerid, uid)
         if boughtPet == true then
