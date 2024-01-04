ad9ff0fe1bced17a7da1e7e813e5f59bad6ab2fc
IPlayps99 <155312670+IPlayps99@users.noreply.github.com>
2024-01-04 14:21:02 +0000

    Update SnipeNoCap.lua

--- SnipeNoCap.lua
+++ SnipeNoCap.lua
@@ -155,7 +155,7 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
                 local unitGems = gems/amount
 		snipeNormal = false
                                  
-                if string.find(item, "Huge") and unitGems <= 5000000 then
+                if string.find(item, "Huge") and unitGems <= 3000000 then
                     coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                     return
                 elseif snipeNormalPets == true and gems == 1 then
