22e2f9e772f15eb09417751c26de334352cb4efa
IPlayps99 <155312670+IPlayps99@users.noreply.github.com>
2024-01-06 22:52:22 +0000

    Update SnipeNoCap.lua

--- SnipeNoCap.lua
+++ SnipeNoCap.lua
@@ -183,7 +183,7 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
                 elseif ((string.find(item, "Key") and not string.find(item, "Lower")) or string.find(item, "Ticket") or string.find(item, "Charm") or class == "Charm") and unitGems <= 2500 then 
                     coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                     return
-                elseif class == "Enchant" and unitGems <= 1000000 then
+                elseif class == "Enchant" and unitGems <= 900000 then
                     if item == "Diamond Chest Mimic" then 
                         coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
                         return
